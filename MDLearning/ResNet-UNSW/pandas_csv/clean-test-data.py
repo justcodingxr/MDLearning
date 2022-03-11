7223a8a7b6e63f8c479cf99784049181d107f52f
justcodingxr <72686280+justcodingxr@users.noreply.github.com>
2022-03-12 07:35:10 +0800

    Add files via upload

---
+++ MDLearning/ResNet-UNSW/pandas_csv/clean-test-data.py
@@ -0,0 +1,124 @@
+import pandas as pd
+#获取特征名列表作为列名
+from columns import columns
+
+dffeature = pd.read_csv("feature.csv")
+featurelist = dffeature['Name']
+df = pd.read_csv("../UNSW_beforeclean/UNSW-4part-testdata.csv", names=featurelist, low_memory=False)
+
+#设置显示格式
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', 200)
+pd.set_option('display.width', 160)
+lie = df.columns.values.tolist()
+print(len(lie))
+print(lie)
+for i in range(45,len(lie)):
+    print('第:',i,'列  ',lie[i])
+    print(df[lie[i]].value_counts())
+print('清洗·============================================================')
+print('根据态势指标将类型转换成对应数字')
+df= df.replace('Normal',0)
+df= df.replace('Analysis',1)
+df= df.replace(' Reconnaissance ',2)
+df= df.replace('Reconnaissance',2)
+df= df.replace('DoS',3)
+df= df.replace(' Fuzzers ',4)
+df= df.replace(' Fuzzers',4)
+df= df.replace('Generic',5)
+df= df.replace(' Shellcode ',6)
+df= df.replace('Shellcode',6)
+df= df.replace('Worms',7)
+df= df.replace('Exploits',8)
+df= df.replace('Backdoor',9)
+df= df.replace('Backdoors',9)
+
+# lie = df.columns.values.tolist()
+# print(len(lie))
+# print(lie)
+# for i in range(45,len(lie)):
+#     print('第:',i,'列  ',lie[i])
+#     print(df[lie[i]].value_counts())
+
+df = df.drop(labels=['srcip','dstip'],axis=1)
+df = df.replace('-','default')
+df = df.fillna(0)
+df = df.replace(' ',0)
+df = df.drop(labels=['Label'],axis=1)
+lie = df.columns.values.tolist()
+print('df long:',len(lie))
+print(lie)
+
+print('独热编码·============================================================')
+dummies1 = pd.get_dummies(df['proto'],prefix='proto')
+l1 = dummies1.columns.values.tolist()
+print('dumies1 long:',len(l1))#这是proto的种类
+print(l1)
+
+dummies2 = pd.get_dummies(df['state'],prefix='state')
+l2 = dummies2.columns.values.tolist()
+print('dumies2 long:',len(l2))
+print(l2)
+
+dummies3 = pd.get_dummies(df['service'],prefix='service')
+l3 = dummies3.columns.values.tolist()
+print('dumies3 long:',len(l3))
+print(l3)
+
+lie2 = df.columns.values.tolist()
+print('df long:',len(lie2))
+print(lie2)
+
+
+print('拼接================================================================')
+
+df1=df[['sport', 'dsport']]
+lie3 = df1.columns.values.tolist()
+print('df1 long:',len(lie3))
+print(lie3)
+
+df2=df[lie2[4:11]]
+lie4 = df2.columns.values.tolist()
+print('df2 long:',len(lie4))
+print(lie4)
+
+df3=df[lie2[12:]]
+lie5 = df3.columns.values.tolist()
+print('df3 long:',len(lie5))
+print(lie5)
+
+#按照列合并
+df=pd.concat([df1,dummies1,dummies2,df2,dummies3 ,df3],axis=1,ignore_index=False)
+lietotal = df.columns.values.tolist()
+print('lietotal long:',len(lietotal))
+print(lietotal)
+
+#扩张维度
+label_df = df[['attack_cat']]  #获取标签
+df = df.drop(labels=['attack_cat'],axis=1) #删除最后一列
+li = ['d0','d1','d2','d3','d4','d5','d6','d7','d8','d9','d10']
+for i in range(11):
+    df.loc[:, li[i]] = 0   #加一列，所有行赋值
+lietotal = df.columns.values.tolist()
+print(lietotal)
+# df=pd.concat([df,label_df],axis=1,ignore_index=False)    #标准化后再拼接
+lietotal = df.columns.values.tolist()
+print('lietotal long:',len(lietotal))
+print(lietotal)
+
+
+
+print('标准化=======================================================================')
+#标准化
+from  sklearn import preprocessing
+scaler=preprocessing.StandardScaler()
+df=scaler.fit_transform(df)                  #已经变成了一个array数组
+df = pd.DataFrame(df,columns=lietotal)              #传入list作为列名，featurelist是一列dataframe
+
+
+#拼接
+df=pd.concat([df,label_df],axis=1,ignore_index=False)
+
+
+df = df.sample(frac=1).reset_index(drop=True)
+df.to_csv('../UNSW_afterclean/UNSW_test.csv',index=False,header=False,encoding="utf_8_sig")
---
+++ MDLearning/ResNet-UNSW/pandas_csv/create-train-data.py
@@ -0,0 +1,67 @@
+#将2,3，的后面四类数据加入4构建训练集a，将a中大量的normal数据删除一些，将a分为训练集和验证集，1选取部分作为测试集。实现样本均匀
+from columns import columns
+import pandas as pd
+dffeature = pd.read_csv("feature.csv")
+featurelist = dffeature['Name']
+df = pd.read_csv("../UNSW_datas/UNSW-NB15_2.csv", names=featurelist, low_memory=False)
+
+
+#设置显示格式
+pd.set_option('display.max_rows', 10)
+pd.set_option('display.max_columns', 200)
+pd.set_option('display.width', 160)
+
+print('数据合并得到新的训练集')
+
+#UNSW-NB15_2.csv,选取多行'Analysis','Backdoor','Worms',' Shellcode '[1342 rows x 49 columns]
+df2 = df.loc[df['attack_cat'].isin(['Analysis','Backdoor','Worms',' Shellcode '])]
+# print(df2)
+
+
+
+#UNSW-NB15_3.csv,选取多行'Analysis','Backdoor','Worms',' Shellcode '[2292 rows x 49 columns]
+df = pd.read_csv("../UNSW_datas/UNSW-NB15_3.csv", names=featurelist, low_memory=False)
+df3 = df.loc[df['attack_cat'].isin(['Analysis','Backdoor','Worms',' Shellcode '])]
+# print(df3)
+
+#将df2,df3合并,[3634 rows x 49 columns]
+df23=pd.concat([df2,df3],axis=0,ignore_index=False)
+lie = df23.columns.values.tolist()
+print(lie)
+print(len(lie))
+
+
+#先将UNSW-NB15_1.csv的Normal数据随机抽取n个暂存df4，再将Normal全部删除，
+# 将df4_part，和UNSW-NB15_1，df（UNSW-NB15_2.csv,UNSW-NB15_3.csv选取多行）合并
+
+df_UNSW1 = pd.read_csv("../UNSW_datas/UNSW-NB15_1.csv", names=featurelist, low_memory=False)  #[700001 rows x 49 columns]
+#选择label为0的正常数据[677786 rows x 49 columns]
+df4 = df_UNSW1.loc[df_UNSW1['Label']==0]
+
+#抽取df4,[10000 rows x 49 columns]
+df4_part =df4.sample(n=10000,  replace=False, weights=None, random_state=None, axis=0)
+
+#[22215 rows x 49 columns]
+df_UNSW1 = df_UNSW1.loc[df_UNSW1['Label'].isin([1])]
+
+#[35849 rows x 49 columns]
+df_traindata=pd.concat([df4_part,df_UNSW1,df23],axis=0,ignore_index=False)
+#获取df.service,df.proto所有列名
+lie = df_traindata.columns.values.tolist()
+print(len(lie))
+print(lie)
+print(df_traindata)
+df_traindata.to_csv('../UNSW_beforeclean/UNSW-23to1-train-raw.csv',index=False,header=False,encoding="utf_8_sig")
+print('清洗·============================================================')
+# Normal            10000
+# Generic            7522
+# Exploits           5409
+# Fuzzers            5051
+# Analysis           2007
+# Reconnaissance     1759
+# DoS                1167
+# Backdoor           1663
+# Shellcode          1140
+# Worms               131
+# Name: attack_cat, dtype: int64
+#[35849 rows x 49 columns]
